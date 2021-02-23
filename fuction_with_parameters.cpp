299c919c81e6a3e8a5a3485b8697138949070784
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-02-23 18:18:10 +0530

    Add files via upload

---
+++ fuction_with_parameters.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+void displaynum ( int n1, float n2)
+{
+  std::cout << " the int number is " << n1;
+  std::cout << " the int number is " << n2;
+  }
+  int main()
+  {
+    int num1=5;
+    double num2=5.5;
+    displaynum (num1,num2);
+    return 0;
+    }
