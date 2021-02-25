1b9478c143bf79d532ed79bfccc191f395b1e852
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-02-25 10:43:05 +0530

    Add files via upload

---
+++ function_prototype.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+int add(int,int);
+int main()
+{
+  int sum;
+  sum=add(100,78);
+  std::cout << " 100 + 78 = " << sum <<'\n';
+  return 0;
+}
+int add (int a, int b)
+{
+return(a+ b)  ;
+}
